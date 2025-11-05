3fb87023f9578b90e6fb31ae2a6b1fb432a538aa
Hyunsu <khs95470039@gmail.com>
2025-11-05 11:26:54 +0900

    화면적용

---
+++ src/main/webapp/assets/js/Login.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const loginBtn = document.getElementById("loginBtn");
+
+  loginBtn.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    window.location.href = "Main.jsp"; // 메인 페이지로 이동
+  });
+});
